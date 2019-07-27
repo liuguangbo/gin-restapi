90266555f737c5ad0076c1e4f1bd8d31dd6659a1
liuguangbo <liu4619774@163.com>
2019-07-27 19:26:37 +0800

    Revert "Revert "first commit""
    This reverts commit e119bed3a32cb2146216e994e63a31e0a73ce928.

---
+++ router.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	. "gin-restapi/apis"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/", IndexApi)
+
+	router.POST("/person", AddPersonApi)
+
+	router.GET("/persons", GetPersonsApi)
+
+	router.GET("/person/:id", GetPersonApi)
+
+	router.PUT("/person/:id", ModPersonApi)
+
+	router.DELETE("/person/:id", DelPersonApi)
+
+	return router
+}
